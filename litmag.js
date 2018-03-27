318a6b6605babdfd694c3666cdaffb0c48b784da
ehslitmag <ehsliterarymag@gmail.com>
2018-03-27 17:04:53 -0500

    Delete litmag.js

--- litmag.js
+++
@@ -1,8 +0,0 @@
-var load = function (){
-	$('header').delay(100).fadeIn(700, "linear");
-	$('.main').delay(800).fadeIn(800, "linear");
-	$('aside').delay(800).fadeIn(800, "linear");
-	$('footer').delay(850).fadeIn(500, "linear");
-};
-
-$(document).ready(load);
